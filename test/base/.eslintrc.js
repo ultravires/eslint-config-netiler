8cb234d92a6034bfff2d744be9b34e742a25a941
ultravires <x555666777@qq.com>
2022-01-27 16:33:34 +0800

    Add test examples

---
+++ test/base/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  root: true
+};
---
+++ test/base/accessor-pairs/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'accessor-pairs': ['error', { setWithoutGet: true, getWithoutSet: false }]
+  }
+};
