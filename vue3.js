b09a96c0c1b7313ac4356313cc66e673987b39c2
ultravires <x555666777@qq.com>
2023-03-06 11:22:05 +0800

    Update rules

--- vue3.js
+++ vue3.js
@@ -1,9 +1,13 @@
 module.exports = {
-  extends: ['./vue.js', 'plugin:vue/vue3-recommended'],
+  extends: ['plugin:vue/vue3-recommended', './vue.js'],
   rules: {
     /**
      * 不允许向 template 模板下添加多个根结点
      */
-    'vue/no-multiple-template-root': 'off'
+    'vue/no-multiple-template-root': 'off',
+    /**
+     * 标签属性过长需要换行
+     */
+    "vue/max-attributes-per-line": 'off'
   }
 };
